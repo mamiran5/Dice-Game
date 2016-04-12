0b5b170d30916e3f67ab3ba1d9a0876c82229f29
Mesaul <mchlsaul@aol.com>
2016-04-11 17:50:59 -0700

    Create Game.java
    There are missing methods within other classes that will need to be implemented for this code to run correctly. A .getDicePairs() method is needed and it will need to return a DicePairs array. Also a .getDice method may be needed in order to use the .rollDice() method in Turn.
    
    Comments will be added to help with clarity of the code's functionality for future versions.

---
+++ Team Project CSE360/src/Game.java
@@ -0,0 +1,111 @@
+package diceGame;
+import java.util.*;
+
+public class Game {
+	
+	public static void main (String [] args){
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter the number of turns you wish to play!");
+		int turnTotal = in.nextInt();
+		
+		System.out.println("Please enter the number of players that will participate in the game!");
+		int playerNum = in.nextInt();
+		
+		Player [] playerList = new Player[playerNum];
+		Turn turns = new Turn();
+		String playerName;
+		
+		for(int playersAdded = 0; playersAdded < playerNum; playersAdded++){
+			System.out.println("Please enter a player name!");
+			playerName = in.next();
+			playerList[playersAdded] = new Player();
+			playerList[playersAdded].setName(playerName);
+			playerList[playersAdded].setScore(500);
+		}
+		
+		for(int turnCount = 0; turnCount < turnTotal; turnCount++){
+			turns.startTurn();
+		}
+		
+		Player winner = playerList[0];
+		ArrayList <Player> tieWinners = new <Player> ArrayList();
+		int tieNumber = 0;
+		
+		for(int playersChecked = 1; playersChecked < playerNum; playersChecked++){
+			int highScore = winner.getScore();
+			int newScore = playerList[playersChecked].getScore();
+			
+			if(newScore == highScore){
+				tieWinners.add(playerList[playersChecked]);
+				tieNumber += 1;
+			}
+			else if(newScore > highScore){
+				winner = playerList[playersChecked];
+				tieWinners.removeAll(tieWinners);
+				tieNumber = 0;
+			}
+		}
+		
+		if(tieNumber != 0){
+			int highestRoll = 0;
+			int newRoll = 0;
+			ArrayList <Player> newTieWinners = new <Player> ArrayList();
+			int newTieNumber = 0;
+			boolean winnerUndecided = true;
+			
+			while(winnerUndecided){
+				
+				turns.rollDice();
+				
+				for(int winningPlayersChecked = 0; winningPlayersChecked < tieNumber; winningPlayersChecked++){
+				
+					for(int numOfPairs = 0; numOfPairs < 3; numOfPairs++){
+						newRoll += tieWinners.get(winningPlayersChecked).getDicePairs()[numOfPairs].getSum();
+					}
+					
+					if(newRoll == highestRoll){
+						newTieWinners.add(tieWinners.get(winningPlayersChecked));
+						newTieNumber += 1; 
+					}
+					else if(newRoll > highestRoll){
+						winner = tieWinners.get(winningPlayersChecked);
+						newTieWinners.removeAll(newTieWinners);
+						newTieNumber = 0;
+					}
+					
+				}
+				
+				if(newTieNumber == 0){
+					winnerUndecided = false;
+				}
+				else{
+					tieNumber = newTieNumber;
+					tieWinners = newTieWinners;
+				}
+				
+			}	
+			
+		}	
+		
+		System.out.println("The winner is " + winner.getName());
+		
+	}
+	
+}
+
+/* Meant for Turn Class
+	public void startTurn(){
+		turns.rollDice();
+		
+		for(int pairsDecided = 0; pairsDecided < playerNum; pairsDecided++){
+			playerList[pairsDecided].pairDice();
+		}
+		
+		// I'm not sure we devised a way to set Dice Pairs for each round
+		
+		for(int roundsFinished = 0; roundsFinished < 3; roundsFinished++){
+			turns.startRound();
+		}
+	}
+*/
