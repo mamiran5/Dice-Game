f939fb9824575c66db8f7a04ddcab434a8144526
mfraiz <mfraiz@asu.edu>
2016-04-29 23:53:23 -0700

    Added files via upload
    Javadoc comments added to gameDriver.

--- Team Project CSE360/src/GameCode/GameDriver.java
+++ Team Project CSE360/src/GameCode/GameDriver.java
@@ -2,13 +2,25 @@ package diceGame;
 
 import java.util.*;
 
+/**This GameDriver Class serves as a link between the GUI and the other driver systems by allowing the GUI to call various 
+ * methods when buttons are pressed as well as access variables of various data classes that are utilized by the dice game.
+ * 
+ * @author mfraiz
+ *
+ */
 public class GameDriver 
 {
 
+	//This list stores the current players
 	private ArrayList<Player> playerList = new ArrayList<Player>();
 	private Turn currentTurn = null;
-	private Player currPlayer = null;
 	
+	
+	/**addPlayer(Player newPlayer)
+	 * 
+	 * Add a player through the welcome screen
+	 * @param newPlayer Player Object to be added.
+	 */
 	public void addPlayer(Player newPlayer)
 	{
 		//Set computer status to false
@@ -16,10 +28,19 @@ public class GameDriver
 		playerList.add(newPlayer);
 	}
 	
+	/**
+	 * Get current players
+	 * @return playerList 	A list of the current Players
+	 */
 	public ArrayList<Player> getCurrPlayers()
 	{
 		return playerList;
 	}
+	
+	/**addComputerPlayers()
+	 * 
+	 * Fills in the rest of the player slots with computer AI players
+	 */
 	public void addComputerPlayers()
 	{
 		while(playerList.size() < 4)
@@ -29,26 +50,43 @@ public class GameDriver
 		}
 	}
 	
+	/**startAturn()
+	 * Signals the start of a new turn and the creation of a turn object to represent that turn
+	 * 
+	 */
 	public void startATurn()
 	{
 		currentTurn = new Turn(playerList);
 	}
 	
+	/**startRolls()
+	 * Starts the rollphase of a turn.
+	 * 
+	 */
 	public void startRolls()
 	{
 		currentTurn.rollDice();
 	}
 	
-	public void startReRolls()
-	{
-		
-	}
-	
+	/**getDiceListRoll
+	 * 
+	 * @param currPlayer	the player who's dice rolls you wish to retrieve
+	 * @param position  the roll you wish to retrieve (0 is the first roll, 5 is the last roll)
+	 * 
+	 * @return Returns a String representation of a player's dice roll indexed by position
+	 */
 	public String getDiceListRoll(Player currPlayer, int position)
 	{
 		return "" + currPlayer.getDiceList().get(position); 
 	}
 	
+	/**getDiceListValue
+	 * 
+	 * @param currPlayer	the player who's dice rolls you wish to retrieve
+	 * @param position  the roll you wish to retrieve (0 is the first roll, 5 is the last roll)
+	 * 
+	 * @return Returns an int representation of a player's dice roll indexed by position
+	 */
 	public int getDiceListValue(Player currPlayer, int position)
 	{
 		return currPlayer.getDiceList().get(position).getValue(); 
